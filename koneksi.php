3b1687456b8f168baa3600b6c5daed6a88211bc2
marzukii <marjukiiahmad@gmail.com>
2018-05-31 19:12:45 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost"; 
+$username= "root";  
+$password= ""; 
+$db_name ="billion_chattbill"; 
+
+//melakukan koneksi ke database
+$kon=new mysqli($host,$username,$password,$db_name);
+
+?>
