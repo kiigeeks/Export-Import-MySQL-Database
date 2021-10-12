a2902995b92a512098161a10f06dc619529f0432
Marzuki <39798637+kiigeeks@users.noreply.github.com>
2021-10-12 14:22:57 +0700

    Update Koneksi
    Update Koneksi ke Database MYSQL

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,15 @@
 <?php
-$host = "localhost"; 
-$username= "root";  
-$password= ""; 
-$db_name ="billion_chattbill"; 
+  //konfigurasi database
+  $host = "localhost"; 
+  $username= "root";  
+  $password= ""; 
+  $db_name = ""; 
 
-//melakukan koneksi ke database
-$kon=new mysqli($host,$username,$password,$db_name);
+  //koneksi ke database
+  $kon = new mysqli($host,$username,$password,$db_name);
 
+  //cek koneksi ke database berhasil atau tidak
+  if (!$kon) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+	}
 ?>
